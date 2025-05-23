fd698bd4307923dd842d9b00647237d15eb5fee1
root <liyinhao0413@qq.com>
2025-05-23 02:39:34 +0000

    Add train.sh

---
+++ vla-scripts/train.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+torchrun \
+  --nproc-per-node="${MLP_WORKER_GPU}" \
+  --nnodes="${MLP_WORKER_NUM}" \
+  --node_rank="${MLP_ROLE_INDEX}" \
+  --master_addr="${MLP_WORKER_0_HOST}" \
+  --master_port="${MLP_WORKER_0_PORT}" \
+  "vla-scripts/train.py" \
+  ${@:1}
